65e2a8a04381f142e908826645ee3bd07da31286
乔栋杰 <qiaodj@asp.citic.com>
2020-05-26 16:36:24 +0800

    修改通知策略

--- src/main/java/com/rails/async/AsyncService.java
+++ src/main/java/com/rails/async/AsyncService.java
@@ -3,7 +3,6 @@ package com.rails.async;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.Callable;
-import java.util.concurrent.TimeUnit;
 
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Service;
 import com.github.rholder.retry.Retryer;
 import com.github.rholder.retry.RetryerBuilder;
 import com.github.rholder.retry.StopStrategies;
-import com.github.rholder.retry.WaitStrategies;
 import com.google.common.base.Predicates;
 
 @Service
@@ -23,8 +21,8 @@ public class AsyncService {
 		retryer = RetryerBuilder.<String>newBuilder().retryIfException() // 抛出异常会进行重试
 				.retryIfResult(Predicates.equalTo(null)) // 如果接口返回的结果不符合预期,也需要重试
 				.retryIfResult(Predicates.equalTo("999999")) // 如果接口返回的结果不符合预期,也需要重试
-				.withWaitStrategy(WaitStrategies.fixedWait(5, TimeUnit.SECONDS)) // 重试策略,
-				.withStopStrategy(StopStrategies.stopAfterAttempt(8)) // 重试次数
+				// .withWaitStrategy(WaitStrategies.fixedWait(5, TimeUnit.SECONDS)) // 重试策略,
+				.withWaitStrategy(new YTWaitStrategy()).withStopStrategy(StopStrategies.stopAfterAttempt(7)) // 重试次数
 				.build();
 	}
 
---
+++ src/main/java/com/rails/async/YTWaitStrategy.java
@@ -0,0 +1,54 @@
+package com.rails.async;
+
+import com.github.rholder.retry.Attempt;
+import com.github.rholder.retry.WaitStrategy;
+
+/**
+ * 异步通知时间间隔
+ * 
+ * @author qiaodj
+ * @version V1.0
+ * @date: 2020年5月26日 上午10:21:14
+ */
+public class YTWaitStrategy implements WaitStrategy {
+
+	@Override
+	public long computeSleepTime(Attempt failedAttempt) {
+		long number = failedAttempt.getAttemptNumber();
+		// if (number == 1) {
+		// return 5 * 1000;// 5秒
+		// } else if (number == 2) {
+		// return 10 * 1000;// 10秒
+		// } else if (number == 3) {
+		// return 30 * 1000;// 30秒
+		// } else if (number == 4) {
+		// return 60 * 1000;// 1分钟
+		// } else if (number == 5) {
+		// return 300 * 1000;// 5分钟
+		// } else if (number == 6) {
+		// return 600 * 1000;// 10分钟
+		// } else if (number == 7) {
+		// return 1800 * 1000;// 30分钟
+		// } else if (number == 8) {
+		// return 7200 * 1000;// 2小时
+		// } else if (number == 9) {
+		// return 14400 * 1000;// 4小时
+		// } else {
+		// return 28800 * 1000;// 8小时
+		// }
+
+		if (number == 1) {
+			return 5 * 1000;// 5秒
+		} else if (number == 2) {
+			return 1 * 1000;// 10秒
+		} else if (number == 3) {
+			return 1 * 1000;// 30秒
+		} else if (number == 4) {
+			return 1 * 1000;// 1分钟
+		} else if (number == 5) {
+			return 1 * 1000;// 5分钟
+		} else {
+			return 1 * 1000;
+		}
+	}
+}
