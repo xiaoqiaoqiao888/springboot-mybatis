04340b70be091f9d4bef62b9d77d50af0b7853df
乔栋杰 <qiaodj@asp.citic.com>
2020-06-30 17:37:08 +0800

    123456

--- src/main/java/com/rails/async/AsyncController.java
+++ src/main/java/com/rails/async/AsyncController.java
@@ -5,30 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rails.timer.NoticeJob;
-import com.rails.timer.NoticeService;
-
 @RestController
 public class AsyncController {
 
 	@Autowired
 	private AsyncService asyncService;
-	@Autowired
-	private NoticeService noticeService;
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam String str) {
 
-		// asyncService.hello();
+		asyncService.hello();
 
-		for (int i = 0; i < 5; i++) {
-
-			NoticeJob noticeJob = new NoticeJob();
-			noticeJob.setJobId(i + "");
-			boolean add = noticeService.add(noticeJob);
-			// noticeService.run();
-			System.out.println(add + ":" + i);
-		}
 		return "SUCCESS";
 	}
 }
